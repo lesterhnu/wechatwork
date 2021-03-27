e3fddfd465da5deafb081807b3dcf2e4c742e41e
lesterhnu <lesterhnu@gmail.com>
2021-03-27 17:52:58 +0800

    update:fill request & response

--- request/statistic.go
+++ request/statistic.go
@@ -1,7 +1,20 @@
 package request
 
 type GetUserBehaviorDataReq struct {
+	UserId    []string `json:"user_id"`
+	Partyid   []int32  `json:"partyid"`
+	StartTime int64    `json:"start_time"`
+	EndTime   int64    `json:"end_time"`
 }
 
 type GetGroupChatStatisticByGroupOwnerReq struct {
+	DayBeginTime int64 `json:"day_begin_time"`
+	DayEndTime   int64 `json:"day_end_time"`
+	OwnerFilter  *struct {
+		UseridList []string `json:"userid_list"`
+	} `json:"owner_filter"`
+	OrderBy  int `json:"order_by"`
+	OrderAsc int `json:"order_asc"`
+	Offset   int `json:"offset"`
+	Limit    int `json:"limit"`
 }
--- response/statistic.go
+++ response/statistic.go
@@ -1,7 +1,33 @@
 package response
 
 type GetUserBehaviorDataResp struct {
+	ErrInfo
+	BehaviorData []*struct {
+		StatTime            int64   `json:"stat_time"`
+		ChatCnt             int     `json:"chat_cnt"`
+		MessageCnt          int     `json:"message_cnt"`
+		ReplyPercentage     float32 `json:"reply_percentage"`
+		AvgReplyTime        int     `json:"avg_reply_time"`
+		NegativeFeedbackCnt int     `json:"negative_feedback_cnt"`
+		NewApplyCnt         int     `json:"new_apply_cnt"`
+		NewContactCnt       int     `json:"new_contact_cnt"`
+	} `json:"behavior_data"`
 }
 
 type GetGroupChatStatisticByGroupOwnerResp struct {
+	ErrInfo
+	Total      int `json:"total"`
+	NextOffset int `json:"next_offset"`
+	Items      []*struct {
+		Owner string `json:"owner"`
+		Data  *struct {
+			NewChatCnt   int `json:"new_chat_cnt"`
+			ChatTotal    int `json:"chat_total"`
+			ChatHasMsg   int `json:"chat_has_msg"`
+			NewMemberCnt int `json:"new_member_cnt"`
+			MemberTotal  int `json:"member_total"`
+			MemberHasMsg int `json:"member_has_msg"`
+			MsgTotal     int `json:"msg_total"`
+		} `json:"data"`
+	} `json:"items"`
 }
